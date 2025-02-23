3757eda6e6ab12ddcf840d5ce8e75e5eeecbf29e
YUTAROOOOOOO <xxx.uzoo.xxx@icloud.com>
2025-02-23 21:49:05 +0900

    Delete src/main directory

--- src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51QsMduBxTf1M0JpPUzyf4fuRLOroHOftszCKGuOvyI6DF3LBhdFtJz8OSIRfZzhxpaTVUtlZF15B7AysB5vV7U6O00SMbyEV9S');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
- stripe.redirectToCheckout({
-   sessionId: sessionId
- })
-});
